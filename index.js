7ddef6371064e889d445db1156175a65139d8641
Andrew Hutchings <andrew.hutchings@gmail.com>
2016-12-12 22:29:32 -0500

    Add Fira Code to the font stack and enable ligatures

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ module.exports.onWindow = browserWindow => browserWindow.setVibrancy('ultra-dark
 
 module.exports.decorateConfig = config => {
   config.backgroundColor = Color(config.backgroundColor).alpha(0.85).rgb().string();
+  config.fontFamily = '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace';
+  config.termCSS = `
+      x-screen x-row {
+          font-variant-ligatures: initial;
+      }
+  `;
   return config;
 }
