813880d70fe5cb865a1317c537202eed3b348735
Andrew Hutchings <andrew.hutchings@gmail.com>
2016-12-12 22:10:07 -0500

    Make the backgroundColor semi-transparent

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const Color = require('color');
+
 module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
+
+module.exports.decorateConfig = config => {
+  config.backgroundColor = Color(config.backgroundColor).alpha(0.5).rgb().string();
+  return config;
+}
