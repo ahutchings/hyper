87b6277b0112907f6d260b3fadf57c04aafcb293
Andrew Hutchings <andrew.hutchings@gmail.com>
2017-09-05 17:34:24 -0400

    Fall back to bash shell when fish cannot be found

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Color = require('color');
 const which = require('which');
 
+const shell = getShell();
+
 module.exports.onWindow = browserWindow =>
   browserWindow.setVibrancy('ultra-dark');
 
@@ -17,7 +19,18 @@ module.exports.decorateConfig = config =>
           font-variant-ligatures: initial;
       }
   `,
-    shell: which.sync('fish', {
-      path: `${process.env.HOME}/brew/bin:${process.env.PATH}`,
-    }),
+    shell,
   });
+
+function getShell() {
+  const path = `${process.env.HOME}/brew/bin:${process.env.PATH}`;
+
+  try {
+    return which.sync('fish', {
+      path,
+    });
+  } catch (error) {
+    console.warn(`Fish shell not found in path ${path}. Falling back to bash shell.`);
+    return which.sync('bash');
+  }
+}
